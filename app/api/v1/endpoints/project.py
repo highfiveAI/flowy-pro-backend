65774bab090362d19764d1d96d6ff934b7ff63e5
kkameoo <khdg1202@naver.com>
2025-06-13 21:05:06 +0900

    Feat: 회의 crud 구현

--- app/api/v1/endpoints/project.py
+++ app/api/v1/endpoints/project.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.db_session import get_db_session
 from app.crud.crud_user import get_all_users
+from app.crud.crud_project import get_project_users_with_projects_by_user_id, get_meetings_with_users_by_project_id
+from uuid import UUID
 router = APIRouter()
 
 @router.post("")
@@ -12,3 +14,13 @@ async def create_projet_api():
 async def list_users(db: AsyncSession = Depends(get_db_session)):
     users = await get_all_users(db)
     return users
+
+@router.get("/user_id/{user_id}")
+async def read_user_projects(user_id: UUID, db: AsyncSession = Depends(get_db_session)):
+    projects = await get_project_users_with_projects_by_user_id(db, user_id)
+    return projects
+
+@router.get("/meeting/{project_id}")
+async def read_meetings_with_users(project_id: UUID, db: AsyncSession = Depends(get_db_session)):
+    meetings = await get_meetings_with_users_by_project_id(db, project_id)
+    return meetings
